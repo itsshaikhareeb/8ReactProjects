cde7b9bda11d6c05c9b98c55978b236a2693a564
Shaikh Areeb <shaikhareeb321@gmail.com>
2026-08-01 12:35:28 +0530

    add features

---
+++ Project4_ImageSlider/src/App.jsx
@@ -0,0 +1,15 @@
+
+import ImageSlider from './components'
+
+function App() {
+  
+
+  return (
+    <>
+      <ImageSlider url={'https://picsum.photos/v2/list'} limit={10} page={1} />
+    </>
+
+  )
+}
+
+export default App
