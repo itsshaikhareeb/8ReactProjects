589bb756b36e15ae617ca21f642371a63e5bcdbf
Shaikh Areeb <shaikhareeb321@gmail.com>
2026-08-01 14:46:42 +0530

    add all functionalities

--- Project5_LoadMoreButton/src/components/index.jsx
+++ Project5_LoadMoreButton/src/components/index.jsx
@@ -4,57 +4,69 @@ import React, { useEffect, useState } from 'react'
 
 function LodeMoreData() {
     const [loading, setLoading] = useState(false)
-    const[products,setProducts]  = useState([])
-    const[count,setCount] = useState(0)
+    const [products, setProducts] = useState([])
+    const [count, setCount] = useState(0)
+    const[disableButton, setDisableButton] = useState(false)
 
-   async function fetchProducts(){
+    async function fetchProducts() {
         try {
             setLoading(true)
-            const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count*20}`)
+            const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`)
             const result = await response.json();
             console.log(result);
 
-            if(result && result.products && result.products.length){
-                setProducts(result.products);
+            if (result && result.products && result.products.length) {
+                setProducts((pre) => [...pre, ...result.products]);
                 setLoading(false)
             }
-        } 
+        }
         catch (e) {
             console.log(e);
             setLoading(false)
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchProducts()
-    },[])
+    }, [count])
 
-    if(loading){
+    useEffect(()=>{
+        if(products && products.length ===100) setDisableButton(true)
+    },[products])
+
+    if (loading) {
         return <div> Loading data, Please Wait!</div>
     }
 
-  return (
+    return (
 
-    <div className='container' >
-        {
-            products && products.length ?
-            products.map((items)=>(
-                <div
-                key={items.id}
-                >
-                    <img 
-                    src={items.thumbnail}
-                    alt={items.title}
-                    />
-                    <p>{items.title}</p>
-                </div>
-            ))
-            :
-            null
-        }
-    </div>
+        <div className='container' >
+            <div className='product-container' >
 
-  )
+                {
+                    products && products.length ?
+                        products.map((items) => (
+                            <div className='product'
+                                key={items.id}
+                            >
+                                <img
+                                    src={items.thumbnail}
+                                    alt={items.title}
+                                />
+                                <p>{items.title}</p>
+                            </div>
+                        ))
+                        :
+                        null
+                }
+            </div>
+            <div>
+                <button disabled={disableButton}  onClick={()=> setCount ( count +1)} className='btn-container' >load more Products</button>
+                {disableButton ? <p>you have seen all 100 produtcs!!!</p>: null}
+            </div>
+        </div>
+
+    )
 }
 
 export default LodeMoreData
